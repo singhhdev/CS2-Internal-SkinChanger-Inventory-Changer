d5217631097d3488562c67929e818ebf1f1dc233
singhhdev <108330780+singhhdev@users.noreply.github.com>
2024-03-21 22:31:37 -0700

    Delete src directory

--- src/sdk/source2-sdk/econ/ceconitemschema.cpp
+++
@@ -1,7 +0,0 @@
-#include "ceconitemschema.hpp"
-
-#include "../../memory/memory.hpp"
-
-bool CPaintKit::UsesLegacyModel() {
-    return static_cast<bool>(reinterpret_cast<std::uintptr_t>(this) + 0xB2);
-}
